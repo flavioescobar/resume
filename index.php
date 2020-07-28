d033b1c98d057c2b3901f3db61b75779c77aa584
Flavio Escobar <flavioescobar1@gmail.com>
2020-07-28 20:56:54 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+<head>
+	<title>Flavio Escobar | Computer Scientist | flavioescobar1@gmail.com</title>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+
+	<meta name="keywords" content="" />
+	<meta name="description" content="" />
+
+	<link rel="stylesheet" type="text/css" href="reset-fonts-grids.css" media="all" />
+	<link rel="stylesheet" type="text/css" href="resume.css" media="all" />
+	<link rel="stylesheet" type="text/css" href="resume-print.css" media="print" />
+</head>
+<body>
+
+<div id="doc2" class="yui-t7">
+	<div id="inner">
+	
+		<div id="hd">
+			<div class="yui-gc">
+				<div class="yui-u first">
+					<h1>Flavio Escobar</h1>
+					<h2>Computer Scientist</h2>
+				</div>
+
+				<div class="yui-u">
+					<div class="contact-info">
+						<h3><a id="pdf" href="CV_Flavio_Escobar.pdf">Download as PDF</a></h3>
+						<h3><a href="mailto:flavioescobar1@gmail.com">flavioescobar1@gmail.com</a></h3>
+						<h3>+55 71 98723-7516</h3>
+					</div><!--// .contact-info -->
+				</div>
+			</div><!--// .yui-gc -->
+		</div><!--// hd -->
+
+		<div id="bd">
+			<div id="yui-main">
+				<div class="yui-b">
+
+					<div class="yui-gf">
+						<div class="yui-u first">
+							<h2>Profile</h2>
+						</div>
+						<div class="yui-u">
+							<p class="enlarge">
+								Professional highly experienced in web and mobile development, always seeking smart and high quality solutions.
+							</p>
+						</div>
+					</div><!--// .yui-gf -->
+
+					<div class="yui-gf">
+						<div class="yui-u first">
+							<h2>Skills</h2>
+						</div>
+						<div class="yui-u">
+
+								<div class="talent">
+									<h2>Web Development</h2>
+									<p>Creation of websites and web applications that are fully responsive (adaptable to any device size).</p>
+								</div>
+
+								<div class="talent">
+									<h2>Mobile Apps</h2>
+									<p>Creation of hybrid mobile apps, compatible with the major mobile operating systems (iOS and Android).</p>
+								</div>
+
+								<div class="talent">
+									<h2>E-commerce</h2>
+									<p>Development of 100% customizable virtual stores, requiring no programming skills from users.</p>
+								</div>
+						</div>
+					</div><!--// .yui-gf -->
+
+					<div class="yui-gf">
+						<div class="yui-u first">
+							<h2>Technical<br />Knowledge</h2>
+						</div>
+						<div class="yui-u">
+							<ul class="talent">
+								<li><strong>PHP</strong></li>
+								<li><strong>Bootstrap</strong></li>
+								<li><strong>Javascript</strong></li>
+								<li class="last"><strong>WordPress</strong></li>
+							</ul>
+
+							<ul class="talent">
+								<li><strong>CSS3</strong></li>
+								<li><strong>MySQL</strong></li>
+                        		<li><strong>jQuery</strong></li>
+								<li class="last"><strong>HTML5</strong></li>
+							</ul>
+
+							<ul class="talent">
+								<li>Shopify</li>
+								<li>Joomla</li>
+								<li>Moodle</li>
+								<li class="last">React Native</li>
+							</ul>
+						</div>
+					</div><!--// .yui-gf-->
+
+					<div class="yui-gf">
+	
+						<div class="yui-u first">
+							<h2>Experience</h2>
+						</div><!--// .yui-u -->
+
+						<div class="yui-u">
+
+	                    <div class="job">
+							<h2>Toptal</h2>
+							<h3>Freelance Senior Software Engineer</h3>
+							<h4>2014 - Today</h4>
+							<p>Website, web application and mobile development for companies all over the world. Working on projects of different sizes and complexities, from simple websites to large scale web applications such as crowdfunding platforms and multi-country marketplaces.</p>
+	                    </div>
+
+	                    <div class="job">
+							<h2>Companhia de Governança Eletrônica do Salvador</h2>
+							<h3>Systems and Advanced Projects Analyst</h3>
+							<h4>2013 - 2015</h4>
+							<p>Working with analysis, development and maintenance of websites, web systems and apps using PHP, Javascript, HTML and CSS languages​​, in addition to jQuery, MySQL database, Joomla CMS and Cordova Platform.</p>
+	                    </div>
+
+	                    <div class="job">
+							<h2>Federação das Indústrias do Estado da Bahia &mdash; FIEB</h2>
+							<h3>CNPq Scholarship - Level B</h3>
+							<h4>2013</h4>
+							<p>Working with research and development in the Innovation Game project. The Innovation Game is a management model closely aligned with the Open Innovation proposal and incorporates the usage of computational tools to support collaboration and knowledge management. The companies' projects are monitored and their innovation results are evaluated, not only by the managers, but by all the employees.</p>
+	                    </div>
+
+						<div class="job">
+							<h2>Virtualize Soluções Digitais Criativas</h2>
+							<h3>Systems Analyst</h3>
+							<h4>2010 - 2012</h4>
+							<p>Developing websites, games and other kinds of applications, mostly with ActionScript 3, PHP and Java. One of the most prominent activities was the development of client-server games with Flash and SmartfoxServer, in addition to mobile games and apps using Starling Framework.</p>
+	                    </div>
+
+	                    <div class="job">
+							<h2>GAPA Bahia</h2>
+							<h3>Web Developer</h3>
+							<h4>2010 - 2012</h4>
+							<p>Development and maintenance of a website and a distance learning environment. These activies were part of the Transversalizando Program. The main technologies and languages used were: Wordpress, Moodle, PHP, MySQL and Flash/ActionScript.</p>
+	                    </div>
+
+	                    <div class="job">
+							<h2>Scientific Initiation &mdash; UFBA</h2>
+							<h3>Scientific Initiation Scholarship</h3>
+							<h4>2009 - 2010</h4>
+							<p>Research and elaboration of a formal specification of a POSIX-compliant filesystem using Z Notation and Z/Eves tool. The filesystem basis and some file and directory operations were specified.</p>
+	                    </div>
+
+						<div class="job">
+							<h2>Centro de Processamento de Dados &mdash; UFBA</h2>
+							<h3>Web Developer (trainee)</h3>
+							<h4>2008 - 2009</h4>
+							<p>Maintenance of Moodle learning platform, implementing PHP modules, creating themes and images, providing courses on how to use Moodle and helping teacher on content insertion and formatting.</p>
+						</div>
+
+	                    <div class="job last">
+							<h2>Net-Escola de Saúde Coletiva &mdash; ISC &mdash; UFBA</h2>
+							<h3>Web Developer (trainee)</h3>
+							<h4>2008</h4>
+							<p>Website maintenance and Moodle courses creation. Some of the activities were: creation of images, animations, themes, folders and dvd covers; content insertion and formatting.</p>
+						</div>
+
+					</div><!--// .yui-u -->
+				</div><!--// .yui-gf -->
+
+
+				<div class="yui-gf last">
+					<div class="yui-u first">
+						<h2>Education</h2>
+					</div>
+					<div class="yui-u">
+						<h2>Federal University of Bahia - Salvador, Bahia</h2>
+						<h3>Graduation completed in 2011</h3>
+					</div>
+				</div><!--// .yui-gf -->
+
+
+				</div><!--// .yui-b -->
+			</div><!--// yui-main -->
+		</div><!--// bd -->
+
+		<div id="ft">
+			<p>Flavio Escobar &mdash; <a href="mailto:flavioescobar1@gmail.com">flavioescobar1@gmail.com</a></p>
+			<p>+55 71 98723-7516</p>
+			<p>Salvador-BA</p>
+		</div><!--// footer -->
+
+	</div><!-- // inner -->
+
+
+</div><!--// doc -->
+
+
+</body>
+</html>
+
